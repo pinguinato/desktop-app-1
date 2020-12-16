f9c111412466542edc79a3dcc975ffc45a13f7f3
Roberto Gianotto <gianottoroberto@gmail.com>
2020-12-16 23:16:46 +0100

    buttons

--- part_manager.py
+++ part_manager.py
@@ -7,6 +7,22 @@ from tkinter import StringVar
 # create window object
 app = Tk()
 
+def populate_list():
+    print("Populate")
+
+def add_item():
+    print("Add")
+
+def remove_item():
+    print("Remove")
+
+def update_item():
+    print("Update")
+
+def clear_text():
+    print("Clear")
+
+
 # Part
 part_text = StringVar()
 part_label = Label(app, text="Part Name", font=("bold",14), pady=20)
@@ -35,13 +51,28 @@ price_label.grid(row=1, column=2, sticky=W) # posizione dell'etichetta
 price_entry = Entry(app, textvariable=price_text)
 price_entry.grid(row=1, column=3) # posizione della casella di testo
 
+# Parts List (list box)
+parts_list = Listbox(app, height=8, width=50, border=0)
+parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
 
+# scrollbar
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+# collegamento alla barra di scorrimento
+parts_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
+# Buttons
+add_btn = Button(app, text="Add Part", width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
 
+remove_btn = Button(app, text="Remove Part", width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
 
+update_btn = Button(app, text="Update Part", width=12, command=update_item)
+update_btn.grid(row=2, column=2)
 
-
-
-
+clear_btn = Button(app, text="Clear Input", width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
 
 # titolo della finestra
 app.title("Part Manager")
